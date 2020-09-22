d082a5830295d69fedda6e765b7e840d78f27718
Vincent Pacheco <vincentpacheco@outlook.com>
2020-09-22 12:16:24 +0200

    Controllers created.

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,25 @@
+class OrdersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def confirmation
+  end
+end
